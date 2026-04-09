c8b2f273a79e3e92a3fa51663cf8f465adc46434
kmpc-575 <kmpconsultancyservices@gmail.com>
2026-04-09 13:16:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Landing from './pages/Landing';
 import Papers from './pages/Papers';
